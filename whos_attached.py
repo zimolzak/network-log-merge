aa9f7017944fc16887bd0973c05c4aa65e244f1d
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2017-01-19 17:39:11 -0500

    Much simplified: cut out debugging type stuff

--- whos_attached.py
+++ whos_attached.py
@@ -3,36 +3,14 @@ import pdb
 
 top_level_url = 'http://routerlogin.net/'
 full_url = top_level_url + 'DEV_show_device.htm'
+username = 'admin'
+password = open('pwd.txt').readline()
+password = password.replace('\n', '')
 
-fancy = True
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+password_mgr.add_password(None, top_level_url, username, password)
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+opener = urllib.request.build_opener(handler)
 
-if fancy:
-
-
-   password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-   username = 'admin'
-   password = open('pwd.txt').readline()
-   password = password.replace('\n', '')
-
-   password_mgr.add_password(None, top_level_url, username, password)
-   handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-   opener = urllib.request.build_opener(handler)
-
-   # pdb.set_trace()
-
-   html = opener.open(full_url).read()
-   print(html)
-   quit()
-
-   urllib.request.install_opener(opener)
-
-   with urllib.request.urlopen(full_url) as response:
-      html = response.read()
-      print(html)
-
-
-else:
-
-   with urllib.request.urlopen(dumb) as response:
-      html = response.read()
-      print(html)
+html = opener.open(full_url).read()
+print(html)

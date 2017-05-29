3ea3ad372077978ee0ed4fbe6fa4d2d164cd6190
Andrew Zimolzak <andyzimolzak@gmail.com>
2017-05-29 09:47:50 -0400

    Minimal error handling, print more data.

--- whos_attached.py
+++ whos_attached.py
@@ -1,7 +1,8 @@
 import urllib.request
 import pdb
+import urllib.error as uerror
 
-top_level_url = 'http://routerlogin.net/'
+top_level_url = 'http://www.routerlogin.net/'
 full_url = top_level_url + 'DEV_show_device.htm'
 username = 'admin'
 password = open('pwd.txt').readline()
@@ -12,12 +13,24 @@ password_mgr.add_password(None, top_level_url, username, password)
 handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
 opener = urllib.request.build_opener(handler)
 
-html = opener.open(full_url).read().decode("utf-8").splitlines()
-#                            bytes -->   str       -->  list
+
+try:
+   html = opener.open(full_url).read().decode("utf-8").splitlines()
+   #                            bytes -->   str       -->  list
+except uerror.HTTPError as h_error:
+    print("Caught error '" + str(h_error) + "'. Quitting.")
+    quit()
 
 for line in html:
    if line.startswith("var attach_device_list"):
+      #print(line)
       attach_device_list = line.replace('var attach_device_list="', '')
       attach_device_list = attach_device_list.replace('";', '').replace('\\', '')
       attach_array = attach_device_list.split(' @#$&*! ')
       print('\n'.join([e.replace(' ', '\t') for e in attach_array]))
+
+print("\n~~~~\n")
+
+for line in html:
+   if line.startswith("var ") and not line.startswith("var attach_device_list"):
+      print(line)

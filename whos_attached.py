824d60ff7b961c075171acee93e0dba9daa95d8f
Andrew Zimolzak <andyzimolzak@gmail.com>
2017-05-29 10:14:49 -0400

    Parsing more variables. Better printout.

--- whos_attached.py
+++ whos_attached.py
@@ -21,16 +21,31 @@ except uerror.HTTPError as h_error:
     print("Caught error '" + str(h_error) + "'. Quitting.")
     quit()
 
+attach_array = []
+junk_lines = []
+attach_dev_mac = []
+deviceIP_name = []
 for line in html:
    if line.startswith("var attach_device_list"):
       #print(line)
       attach_device_list = line.replace('var attach_device_list="', '')
-      attach_device_list = attach_device_list.replace('";', '').replace('\\', '')
+      attach_device_list = attach_device_list.replace(' @\#$\&*!";', '').replace('\\', '')
       attach_array = attach_device_list.split(' @#$&*! ')
-      print('\n'.join([e.replace(' ', '\t') for e in attach_array]))
+   elif line.startswith("var attach_dev_mac"):
+      attach_dev_mac.append(line)
+   elif line.startswith("var deviceIP_name") and not line.startswith("var deviceIP_name_num"):
+      deviceIP_name.append(line)
+   elif line.startswith("var "):
+      junk_lines.append(line)
 
-print("\n~~~~\n")
+print("attach_device_list\n====")
+print('\n'.join([e.replace(' ', '\t') for e in attach_array]) + '\n')
 
-for line in html:
-   if line.startswith("var ") and not line.startswith("var attach_device_list"):
-      print(line)
+print("attach_dev_mac\n====")
+print('\n'.join(attach_dev_mac) + '\n')
+
+print("deviceIP_name\n====")
+print('\n'.join(deviceIP_name) + '\n')
+
+print("junk lines\n====")
+print("\n".join(junk_lines) + '\n')
